a07eb05972ccc2a60e5e0aeb258fff10b8472be2
Kelli Margaret & Sam Martinez <student@epicodus.com>
2015-08-27 13:50:00 -0700

    add getall test and pass

--- tests/CheckoutTest.php
+++ tests/CheckoutTest.php
@@ -38,6 +38,26 @@
             $result = Checkout::getAll();
             $this->assertEquals([$new_checkout], $result);
         }
+
+        function testGetAll()
+        {
+            //Arrange
+            $id = null;
+            $due_date = "2012-12-12";
+            $test_checkout = new Checkout($id, $due_date);
+            $test_checkout->save();
+
+
+            $due_date2 = "1990-07-26";
+            $test_checkout2 = new Checkout($id, $due_date2);
+            $test_checkout2->save();
+
+            //Act
+            $result = Checkout::getAll();
+
+            //Assert
+            $this->assertEquals([$test_checkout, $test_checkout2], $result);
+        }
     }
 
 
