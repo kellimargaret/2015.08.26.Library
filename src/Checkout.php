dbb28daf0e9b2c3ceb5eabfbca4f91cd253459a0
Kelli Margaret & Sam Martinez <student@epicodus.com>
2015-08-27 14:01:21 -0700

    add find and update tests and pass

--- src/Checkout.php
+++ src/Checkout.php
@@ -34,6 +34,12 @@
             $this->id = $GLOBALS['DB']->lastInsertId();
         }
 
+        function update($new_due_date)
+        {
+            $GLOBALS['DB']->exec("UPDATE checkouts SET due_date = '{$new_due_date}' WHERE id = {$this->getId()};");
+            $this->setDueDate($new_due_date);
+        }
+
         static function getAll()
         {
             $returned_checkouts = $GLOBALS['DB']->query("SELECT * FROM checkouts;");
@@ -53,6 +59,20 @@
         {
             $GLOBALS['DB']->exec("DELETE FROM checkouts;");
         }
+
+        static function find($search_id)
+        {
+            $found_checkout = null;
+            $checkouts = Checkout::getAll();
+            foreach ($checkouts as $checkout) {
+                $checkout_id = $checkout->getId();
+                if ($checkout_id == $search_id) {
+                    $found_checkout = $checkout;
+                }
+            }
+            return $found_checkout;
+        }
+
     }
 
 
--- tests/CheckoutTest.php
+++ tests/CheckoutTest.php
@@ -58,6 +58,65 @@
             //Assert
             $this->assertEquals([$test_checkout, $test_checkout2], $result);
         }
+
+        function testDeleteAll()
+        {
+            //Arrange
+            $id = null;
+            $due_date= "2012-12-12";
+            $test_checkout = new Checkout($id, $due_date);
+            $test_checkout->save();
+
+            $due_date2 = "1989-09-04";
+            $test_checkout2 = new Checkout($id, $due_date2);
+
+            $test_checkout2->save();
+
+            //Act
+            Checkout::deleteAll();
+            $result = Checkout::getAll();
+
+            //Assert
+            $this->assertEquals([], $result);
+        }
+
+
+        function testUpdate()
+        {
+            //Arrange
+            $id = null;
+            $due_date = "2012-12-12";
+            $test_checkout = new Checkout($id, $due_date);
+            $test_checkout->save();
+
+            $new_due_date = "2013-03-05";
+
+            //Act
+            $test_checkout->update($new_due_date);
+
+            //Assert
+            $result = $test_checkout->getDueDate();
+            $this->assertEquals($new_due_date, $result);
+        }
+
+        function testFind()
+        {
+            //Arrange
+            $id = null;
+            $due_date = "2012-12-12";
+            $test_checkout = new Checkout($id, $due_date);
+            $test_checkout->save();
+
+            $due_date2 = "1989-09-04";
+            $test_checkout2 = new Checkout($id, $due_date2);
+            $test_checkout2->save();
+
+            //Act
+            $result = Checkout::find($test_checkout->getId());
+
+            //Assert
+            $this->assertEquals($test_checkout, $result);
+        }
     }
 
 
